5d18270b81d8e69c54f4c7a5d8e70fae051a777a
Brendan Hanna and Brian Rush <brendan+brian@>
2017-05-25 15:30:15 -0700

    add javascript that sorts movies by rating upon click of rating table header.  does not yet display sorted results in table

--- js/script.js
+++ js/script.js
@@ -1,10 +1,43 @@
+//Back End//
+function Library() {
+  this.movieArray = [];
+}
+
+function Movie(mtitle, year, rating) {
+  this.mtitle = mtitle;
+  this.year = year;
+  this.rating = rating;
+}
+
+var library = new Library();
+
+
+//Front End//
+
 $(document).ready(function(){
-  $("#tomato-form").submit (function(event){
+  $("#tomato-form").submit(function(event){
     event.preventDefault();
 
+    var inputtedTitle = $("input#movie-title").val();
+    var inputtedYear = $("input#movie-year").val();
+    var inputtedRating = parseInt($("input#movie-rating").val());
+    // var parsedRating = parseInt(inputtedRating)
 
+    var newMovie = new Movie(inputtedTitle, inputtedYear, inputtedRating);
+
+    $("#movie-table").append("<tr> <td>" + newMovie.mtitle + "</td> <td>" + newMovie.year + "</td> <td>" + newMovie.rating + "</td> </tr>");
+
+    library.movieArray.push(newMovie);
+
+
+  });
+
+  $("#sort-button").click(function() {
+
+    library.movieArray.sort(function(a, b){
+      return a.rating - b.rating
+    });
 
   })
 
-
-})
+});
